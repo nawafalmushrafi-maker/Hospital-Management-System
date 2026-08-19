27f7adba85cf5757aebfdf211d93a7149164c485
nawafalmushrafi-maker <nawafalmushrafi@gmail.com>
2026-08-19 09:49:47 +0400

    Person getters/setters [Task 1.1]

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.security.KeyStore;
+
 public class Person {
     // Attributes
     private String id;
@@ -13,6 +15,7 @@ public class Person {
     private String nationalId;
     private int age;
     private boolean activeStatus;
+    private boolean active;
 
 
     // Full constructor______________
@@ -55,6 +58,98 @@ public class Person {
                 true
         );
 
+
+    }
+    // Setters
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+
+// Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isActive() {
+        return active;
     }
 
 
